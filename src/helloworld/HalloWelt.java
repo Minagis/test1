7ee6c859054ec4e9033406d65cf7290dd5a61cdc
Minagis <Yoshimiru1@yandex.ru>
2017-06-29 02:06:14 +0300

    First commit

---
+++ src/helloworld/HalloWelt.java
@@ -0,0 +1,10 @@
+package helloworld;
+
+/**
+ * Created by Yoshimiru on 29.06.2017.
+ */
+public class HalloWelt {
+    public static void main(String[] args) {
+        System.out.println("2 часа ночи черт побери!!");
+    }
+}
